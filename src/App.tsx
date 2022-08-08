f46b40f9715b47a6e8cbbc4a77fd47834e829e8e
berkbaysal <george@do.com>
2022-08-08 13:17:00 +0300

    proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import * as React from "react"
+import {useState} from "react"
+import Bahrain from "./assets/img/tracks/bahrain-track-2.svg"
+import DRS from "./assets/img/tracks/bahrain-drs.svg"
+import BahrainSectors from "./assets/img/tracks/bahrain-track-sectors.svg"
+
+
 
 function App() {
 
-    return ( <div></div> );
+    const [currentView, setCurrentView] = useState<number>(0)
+
+    const availableViews:string[] = ["Track","Sectors"]
+
+    const changeView =()=>{
+        if(currentView === 0){
+            setCurrentView(1);
+        }
+        else{
+            setCurrentView(0);
+        }
+    }
+
+    return (
+        <div className="track-container">
+            
+            {currentView===0 &&<Bahrain className="track"/>}
+            {currentView===1 && <BahrainSectors className="track" />}
+
+            <button onClick={changeView}>Swap</button>
+        </div>);
 }
 
 export default App;
